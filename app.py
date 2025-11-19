eed372e078b1c829a26a6a1562bdc7e89b23bc96
hassanbem <hassanm7782@gmail.com>
2025-11-19 02:29:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
+@app.route("/", methods=["GET", "POST"])
+def index():
+    recommendations = []
+    query = ""
+    if request.method == "POST":
+        query = request.form.get("movie")
+        recommendations = recommend_movie(query, 5)
+    return render_template("index.html", recommendations=recommendations, query=query)
 
+# ----------------------------
+# 4. Run App
+# ----------------------------
+if __name__ == "__main__":
+    app.run(debug=True)
